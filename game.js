6f25c785a02050952c35bbe1bda3db6f0c7f7a90
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-12-05 02:23:54 +0200

    games handling

--- game.js
+++ game.js
@@ -2,9 +2,50 @@ const divWindow = document.getElementById("window");
 const h1 = document.getElementsByClassName("start__title")[0];
 const h6 = document.getElementById("allTries");
 
-let won = localStorage.getItem("won") || false;
-let lose = localStorage.getItem("lose") || false;
-let start = localStorage.getItem("start") || false;
+// store url param ?game
+let url = new URL(window.location.href);
+let game = url.searchParams.get("game");
+
+let won;
+let lose;
+let start;
+
+won = localStorage.getItem("won") || false;
+lose = localStorage.getItem("lose") || false;
+start = localStorage.getItem("start") || false;
+
+// games logic
+
+let games = [
+  "rft5t6y7uhjbgyut67ihkjbgyu7iujoknbhgyuhijkn", // 1
+  "ihuiy8oi9jlhukyigi7i8uhjgvfcrdtfyrdtges4tde", // 2
+  "98765rer4tsdfrcgvhtyuhijoklmjnjbhvgcfdrtfyi", // 3
+  "98u9y7t6ur5ye43w2esrdtfygu787u9i0opkljnbhvg", // 4
+  "kio9jklnhuiy7hbjgyutgfvhhftrfgcdrte4dfrs34w", // 5
+];
+
+let playedGames = localStorage.getItem("playedGames") || [];
+
+let gameFail =
+  game === null ||
+  !(
+    games.filter((e, i) => {
+      if (e === game) {
+        if (localStorage.getItem(game) === null) {
+          localStorage.setItem(game, i + 1);
+          localStorage.removeItem("won");
+          localStorage.removeItem("lose");
+          localStorage.removeItem("start");
+          localStorage.removeItem("tries");
+          localStorage.removeItem("guesses");
+          localStorage.removeItem("digit");
+          window.location.reload();
+        }
+        return true;
+      }
+    }).length > 0
+  ) ||
+  false;
 
 divWindow.addEventListener("click", handelEvent);
 h1.innerHTML = "Start";
@@ -15,17 +56,28 @@ function handelEvent() {
   h1.innerHTML = "";
   h1.classList.add("hide");
 }
-if (won || lose || start) {
+if (won || lose || start || gameFail) {
   divWindow.removeEventListener("click", handelEvent);
   divWindow.classList.remove("start");
 
   h1.innerHTML = "";
 
-  if (won) {
+  if (gameFail) {
+    h1.innerHTML = "Game not found";
+    h1.classList.remove("hide");
+    divWindow.classList.add("lose");
+  } else if (won) {
     h1.classList.remove("hide");
     console.log("won");
     divWindow.classList.add("won");
-    h1.innerHTML = `You won <br /> 🎉 <br /> only ${10 - tries}`;
+    h1.innerHTML = `You won <br /> 🎉 `;
+    h6.innerHTML = "only " + (10 - tries) + " tries";
+    // set h1 after content to localStorage.getItem(game)
+    h1.classList.add("after");
+    h1.style.setProperty(
+      "--after-content",
+      '"Game ' + localStorage.getItem(game) + ' "'
+    );
   } else if (lose) {
     h1.classList.remove("hide");
     console.log("lose");
